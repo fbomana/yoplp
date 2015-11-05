a3f50d37b3a91bd2a6ad372e989bd17e8de158c1
fbomana <1fbomana1>
2015-11-05 10:18:11 +0100

    Cambios incompletos para transformar la actividad de preferencias en una actividad normal

--- app/src/main/java/es/ait/yoplp/settings/YOPLPSettingsActivity.java
+++ app/src/main/java/es/ait/yoplp/settings/YOPLPSettingsActivity.java
@@ -2,6 +2,7 @@ package es.ait.yoplp.settings;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
 
 import es.ait.yoplp.R;
 
@@ -10,13 +11,13 @@ import es.ait.yoplp.R;
  *
  * TO-DO: cambiarla para que use fragments y evitar el probrlema con el método depricated.s
  */
-public class YOPLPSettingsActivity extends PreferenceActivity
+public class YOPLPSettingsActivity extends AppCompatActivity
 {
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+
     }
 
 }
