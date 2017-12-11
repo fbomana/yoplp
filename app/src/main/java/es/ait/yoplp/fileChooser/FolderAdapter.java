d71d58986d4de8ab86713a64804b654b7c4c530e
fbomana <fbomana@gmail.com>
2017-12-11 19:52:32 +0100

    Regularización de las ramas

--- app/src/main/java/es/ait/yoplp/fileChooser/FolderAdapter.java
+++ app/src/main/java/es/ait/yoplp/fileChooser/FolderAdapter.java
@@ -97,7 +97,19 @@ class FolderAdapter extends ArrayAdapter
                     {
                         result.add( file );
                     }
+                    
                 }
+                
+                aux = new File("/mnt").listFiles();
+
+                for (File anAux : aux)
+                {
+                    if (anAux.getName().startsWith("sdcard") )
+                    {
+                        result.add(anAux);
+                    }
+                }
+
             }
         }
         return result;
